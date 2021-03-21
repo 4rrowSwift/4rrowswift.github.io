6c2b6f322ad2ed56fb20f8a45a21db6e207fa9bd
4rrowSwift <4rrowSwift@gmail.com>
2021-03-22 00:23:11 +0400

    last update

--- js/script.js
+++ js/script.js
@@ -11,3 +11,14 @@ burger.onclick = function () {
 		list.remove('_active');
 	}
 }
+function ibg() {
+
+	let ibg = document.querySelectorAll(".ibg");
+	for (var i = 0; i < ibg.length; i++) {
+		if (ibg[i].querySelector('img')) {
+			ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+		}
+	}
+}
+
+ibg();
